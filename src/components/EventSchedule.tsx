2b8a26c5a1a18dad9401aec2287cd504abd85c50
Sujay V Kulkarni <155747848+SujayKulkarni-2211@users.noreply.github.com>
2025-02-25 21:00:12 +0530

    Create EventSchedule.tsx

---
+++ src/components/EventSchedule.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./EventSchedule.css";
+
+const EventSchedule: React.FC = () => {
+  return (
+    <section className="schedule">
+      <h2>Event Schedule</h2>
+      <p>Details about session timings...</p>
+    </section>
+  );
+};
+
+export default EventSchedule;
