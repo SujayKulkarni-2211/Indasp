fb0e40b5a69d01ea2ae96164aafabbb7db220a85
Sujay V Kulkarni <155747848+SujayKulkarni-2211@users.noreply.github.com>
2025-02-25 20:59:47 +0530

    Create Team.tsx

---
+++ src/components/Team.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Team.css";
+
+const Team: React.FC = () => {
+  return (
+    <section className="team">
+      <h2>Meet the Team</h2>
+      <p>Our dedicated team of data science enthusiasts!</p>
+    </section>
+  );
+};
+
+export default Team;
