f1f39c54942b979b3010c657d9f3927bcc9c66b2
Sujay V Kulkarni <155747848+SujayKulkarni-2211@users.noreply.github.com>
2025-02-25 20:59:27 +0530

    Create Trainer.tsx

---
+++ src/components/Trainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Trainer.css";
+
+const Trainer: React.FC = () => {
+  return (
+    <section className="trainer">
+      <h2>Trainer</h2>
+      <img src="/ananth_pullur.jpg" alt="Ananth Pullur" className="trainer-img" />
+    </section>
+  );
+};
+
+export default Trainer;
